86d42acf23b09750472641ddcfb82b78bcb70693
Hardik Mehta <imt_2021039@iiitm.ac.in>
2023-12-16 22:08:39 +0530

    Updated files

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const connectToMongo = require('./db');
 const authRouter = require('./routes/auth');
 const NotesRouter = require('./routes/notes');
@@ -7,6 +9,7 @@ const port = 5000;
 connectToMongo();
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json()); //middleware used to access body of request.
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
